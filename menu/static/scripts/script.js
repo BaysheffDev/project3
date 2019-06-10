6f40664a769c50b915a794615fb7b1fcf6062c68
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2019-06-10 18:00:40 +0930

    continued work on styling and animations

--- menu/static/scripts/script.js
+++ menu/static/scripts/script.js
@@ -1,1 +1,39 @@
 console.log("Hello, World!");
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Grab useful DOM elements
+  const cartButton = document.querySelector('.cart_button');
+  const cartIcon = document.querySelector('.cart_icon');
+  const cart = document.querySelector('.cart_contents');
+  const cartOverlay = document.querySelector('.cart_overlay');
+
+  // Cart functionality
+  let cartOpen = false;
+  cartButton.addEventListener("click", function() {
+    if (cartOpen) {
+      cartOverlay.classList.add('hidden');
+      cart.classList.remove('active');
+      cart.classList.add('hidden');
+      cartButton.classList.remove('cart_button_active');
+      cartIcon.classList.remove('cart_icon_active');
+      cartOpen = false;
+    }
+    else {
+      cartOverlay.classList.remove('hidden');
+      cart.classList.remove('hidden');
+      cart.classList.add('active');
+      cartButton.classList.add('cart_button_active');
+      cartIcon.classList.add('cart_icon_active');
+      cartOpen = true;
+    }
+  });
+
+});
+
+// Trigger cart notifications
+function cartNotification() {
+  notification = document.querySelector('.cart_notification_container');
+  notification.classList.remove('cart_notification_animation');
+  void notification.offsetWidth;
+  notification.classList.add('cart_notification_animation');
+}
