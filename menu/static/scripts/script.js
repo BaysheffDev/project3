3da8779a83f649b080e8c05c56a86f66c1fa2bb9
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2019-06-16 00:36:07 +0930

    - Moved cheese as subs category topping to an extra for each item - Close toppings for an item when clicking minus and qty is 0 - Display toppings across 2 rows - Add photos for categories - Added night mode toggle to footer - Stacked pasta and subs categories because small - Styling adjustments

--- menu/static/scripts/script.js
+++ menu/static/scripts/script.js
@@ -1,4 +1,3 @@
-console.log("Hello, World!");
 
 document.addEventListener('DOMContentLoaded', () => {
   // Grab useful DOM elements
@@ -6,6 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const cartIcon = document.querySelector('.cart_icon');
   const cart = document.querySelector('.cart_contents');
   const cartOverlay = document.querySelector('.cart_overlay');
+  const nightMode = document.querySelector('.switch input');
+  const slider = document.querySelector('.slider');
+
+  // Nightmode color toggle
+  slider.onclick = () => {
+    if (nightMode.checked === false) {
+      document.querySelector('body').style.background = "black";
+      document.querySelector('body').style.color = "white";
+      console.log('night');
+    }
+    else {
+      document.querySelector('body').style.background = "white";
+      document.querySelector('body').style.color = "black";
+      console.log('day');
+    }
+  }
 
   // Cart functionality
   let cartOpen = false;
@@ -37,58 +52,67 @@ document.addEventListener('DOMContentLoaded', () => {
     cartOpen = false;
   }
 
-  // Add functionality to + button
+  // Add functionality to + button.
+  // Increments qty and disables other items if > 0 for either size
   document.querySelectorAll('.plus').forEach(plus => {
     plus.addEventListener('click', function() {
-      console.log("plus");
       const qty = this.closest('.quantity_container').children[1];
       let val = qty.value;
       val++;
       qty.value = val;
-      if (val === 1) {
+      // Check if item has it's own exclusive toppings.
+      // If yes, don't disable other items and reveal toppings
+      const category_toppings = this.closest('.category_box').querySelector('.category_toppings');
+      const item_toppings = this.closest('.item_box').querySelector('.item_toppings');
+      if (val === 1 && category_toppings !== null) {
         const thisItem = this.closest('.item').querySelector('.item_overlay');
         const otherItems = this.closest('.item_container').querySelectorAll('.item_overlay');
-        console.log(otherItems);
         otherItems.forEach(item => {
           item.classList.remove('hidden');
           item.classList.add('active');
         });
         thisItem.classList.remove('active');
         thisItem.classList.add('hidden');
+        // Expand toppings
+      }
+      else if (item_toppings !== null) {
+        const height = item_toppings.querySelector('.toppings_box').offsetHeight;
+        item_toppings.style.height = `${height}px`;
       }
     });
   });
-  // Add functionality to - button
+  // Add functionality to - button.
+  // Decreases qty and enables other items if 0
   document.querySelectorAll('.minus').forEach(minus => {
     minus.addEventListener('click', function() {
-      console.log("minus");
       const qty = this.closest('.quantity_container').children[1];
       const item_quantities = this.closest('.price_container').querySelectorAll('input');
-      console.log(item_quantities[0].value);
       let val = qty.value;
       val--;
       if (val >= 0) {
         qty.value = val;
       }
-      if (item_quantities[0].value == 0 && item_quantities[1].value == 0) {
+      const toppings = this.closest('.item_box').querySelector('.item_toppings');
+      function checkQtys(qtys) {
+        for (let i = 0, len = qtys.length; i < len; i++) {
+          if (qtys[i].value != 0) {
+            return false;
+          }
+        }
+        return true;
+      }
+      if (checkQtys(item_quantities)) {
         const otherItems = this.closest('.item_container').querySelectorAll('.item_overlay');
-        console.log(otherItems);
         otherItems.forEach(item => {
           item.classList.remove('active');
           item.classList.add('hidden');
         });
+        if (toppings !== null) {
+          toppings.style.height = "0px";
+        }
       }
     });
   });
-  // If user selects an item, expand the related toppings or extras
-  // document.querySelectorAll('.quantity_container input').forEach(qty => {
-  //   qty.addEventListener('change', function() {
-  //     console.log("qty");
-  //     let val = this.closest('.quantity_container').children[1].value;
-  //     val++;
-  //     this.closest('.quantity_container').children[1].value = val;
-  //   });
-  // });
 
 });
 
